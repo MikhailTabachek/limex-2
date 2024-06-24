4f5db479a2e9ae50a1e802f97acfa4ef9c576b75
Mikhail <mishanyaveselyi@gmail.com>
2024-06-23 21:30:13 -0400

    bug fixed (use client)

--- app/components/SettingsMenu.tsx
+++ app/components/SettingsMenu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { FaAngleDown } from 'react-icons/fa';
 import CompactRangeSlider from './CompactRangeSlider';
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import "../app/globals.css";
 import Link from 'next/link';
